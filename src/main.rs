2a66f0f455b92dbb70f5ed977f855568d769dec5
Casey Pak <casey.pak@hkevpower.com>
2024-07-03 10:09:02 +0800

    init physic_obj

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,25 @@
+pub mod physic_obj;
+
 use macroquad::prelude::*;
 
 #[macroquad::main("tsq")]
 async fn main() {
-    let mut circle_1 = CircleBody {
+
+    let mut obj_1 = physic_obj::Actor {
+        actor_type: physic_obj::ActorType::Circle,
         x_pos: screen_height() / 2.0,
         y_pos: screen_height() / 2.0,
+        width: 15.0,
+        height: 15.0,
         radius: 15.0,
         color: YELLOW
     };
 
     loop {
         clear_background(LIGHTGRAY);
-
-        if is_key_down(KeyCode::Right) {
-            circle_1.x_pos += 1.0;
-        }
-        if is_key_down(KeyCode::Left) {
-            circle_1.x_pos -= 1.0;
-        }
-        if is_key_down(KeyCode::Down) {
-            circle_1.y_pos += 1.0;
-        }
-        if is_key_down(KeyCode::Up) {
-            circle_1.y_pos -= 1.0;
-        }
-
-        circle_1.draw();
+        obj_1.run();
         next_frame().await
     }
 }
 
-struct CircleBody {
-    x_pos: f32,
-    y_pos: f32,
-    radius: f32,
-    color: Color
-}
-
-impl CircleBody {
-    fn draw(&self) {
-        draw_circle(self.x_pos, self.y_pos, self.radius, self.color)
-    }
-}
     
---
+++ src/physic_obj.rs
@@ -0,0 +1,46 @@
+use macroquad::prelude::*;
+
+#[derive(PartialEq)]  
+pub enum ActorType {
+    Circle,
+    Rectangle
+}
+
+pub struct Actor {
+    pub actor_type : ActorType,
+    pub x_pos : f32,
+    pub y_pos : f32,
+    pub width : f32,
+    pub height: f32,
+    pub radius: f32,
+    pub color : Color
+}
+
+impl Actor {
+    fn draw(&mut self) {
+
+        if self.actor_type == ActorType::Rectangle {
+            draw_rectangle(self.x_pos, self.y_pos, self.width, self.height, self.color)
+        }
+        
+        if self.actor_type == ActorType::Circle {
+            draw_circle(self.x_pos, self.y_pos, self.radius, self.color)
+        }
+    }
+
+    pub fn run(&mut self) {
+        if is_key_down(KeyCode::Right) || is_key_down(KeyCode::D) {
+            self.x_pos += 1.0;
+        }
+        if is_key_down(KeyCode::Left) || is_key_down(KeyCode::A) {
+            self.x_pos -= 1.0;
+        }
+        if is_key_down(KeyCode::Down) || is_key_down(KeyCode::S) {
+            self.y_pos += 1.0;
+        }
+        if is_key_down(KeyCode::Up) || is_key_down(KeyCode::W) {
+            self.y_pos -= 1.0;
+        }
+        self.draw();
+    }
+}
